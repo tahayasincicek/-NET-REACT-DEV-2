6ee58da7c7a4ceb6f51ce3b9253463bd0c6b61a0
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-03-01 00:33:43 +0300

    Add files via upload

---
+++ 11/11/Program.cs
@@ -0,0 +1,15 @@
+﻿List<int> grades = new List<int> { 45, 78, 32, 90, 50, 48, 67, 20 };
+
+for (int i = 0; i < grades.Count; i++)
+{
+    if (grades[i] < 50)
+    {
+        grades[i] = 50;
+    }
+}
+
+Console.WriteLine("Updated grades:");
+foreach (int grade in grades)
+{
+    Console.Write(grade + " ");
+}
