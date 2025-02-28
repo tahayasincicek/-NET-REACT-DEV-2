2d0a5d52ee5852d51ae2e35025285e43476890a7
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-03-01 00:27:09 +0300

    Add files via upload

---
+++ 1/Sayıların Toplamı/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+number = Math.Abs(number); //takes the absolute value of negative numbers
+
+
+for (; number > 0; number /= 10)
+{
+    sum += number % 10;
+}
+Console.WriteLine("Sum of the numbers: " + sum);
---
+++ 2/2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+
+    Console.WriteLine("Enter a number between 10 and 100");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number >= 10 && number <= 100)
+    {
+        Console.WriteLine("True range");
+        break;
+    }
+
+    Console.WriteLine("Please enter true number");
+}
