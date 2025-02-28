472bc539dfd0182be26a15b19b1547f25d707f26
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-03-01 00:30:01 +0300

    Add files via upload

---
+++ 5/the longest and shortes words/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] words = { "apple", "banana", "strawberry", "kiwi", "melon" };
+string longest = words[0];
+string shortest = words[0];
+foreach(string word in words)
+{
+    if (word.Length > longest.Length)
+    {
+        longest = word;
+    }
+
+    
+}
+foreach(string word2 in words)
+{
+    if (word2.Length < shortest.Length)
+    {
+        shortest = word2;
+    }
+
+}
+Console.WriteLine("Longest word is : " + longest);
+Console.WriteLine("Shortest word is : " + shortest);
---
+++ 6/6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Write a sentence: ");
+string sentence = Console.ReadLine();
+
+string[] words = sentence.Split(" ");
+
+Array.Sort(words);
+
+Console.WriteLine("Words in alphabetical order: ");
+
+foreach(string word in words)
+{
+    Console.WriteLine(word);
+}
