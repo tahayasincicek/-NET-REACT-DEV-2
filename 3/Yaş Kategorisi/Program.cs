cd68fbe95ef674c5a703571528b8b0827a0f089e
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-03-01 00:28:56 +0300

    Add files via upload

---
+++ 3/Yaş Kategorisi/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter the number of people whose age you want to input: ");
+int size = int.Parse(Console.ReadLine());
+int[] ageList = new int[size];
+
+for(int i = 0; i < size; i++)
+{
+    Console.Write($"Enter {i+1}. age: ");
+    ageList[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+foreach(int age in ageList)
+{
+    string category;
+   
+    if(age>=0 && age <= 12)
+    {
+        category = "Child";
+    }
+
+    else if(age >= 13 && age <= 19)
+    {
+        category = "Teenager";
+    }
+
+    else if(age >= 20 && age <= 64)
+    {
+        category = "Adult";
+    }
+
+    else
+        category = "Old"; 
+    
+    Console.WriteLine($"Age: {age}, Category: {category}");
+}
---
+++ 4/Repeating Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = { 5, 3, 7, 3, 9, 5, 3, 1, 5, 7, 8 };
+bool[] visited = new bool[array.Length];
+
+Console.WriteLine("Duplicate elements:");
+for (int i = 0; i < array.Length; i++)
+{
+    if (visited[i]) continue;
+
+    int count = 1;
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[i] == array[j])
+        {
+            visited[j] = true;
+            count++;
+        }
+    }
+
+    if (count > 1)
+        Console.WriteLine($"{array[i]} ({count} times)");
+}
